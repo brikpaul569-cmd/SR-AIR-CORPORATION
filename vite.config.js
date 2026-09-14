cbf8fc64e824d3d9672199bd5ca6c2c8c574ed62
brikpaul569-cmd <brikpaul569@gmail.com>
2026-09-14 14:20:39 -0500

    feat(deploy): add Vercel deployment config (root base, SPA rewrite, robots.txt)

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/SR-AIR-CORPORATION/',
+  base: '/',
   plugins: [react()],
 })
